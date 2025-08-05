033cbecc40af4c47ddacec8d4d333c4d65107c3e
IMaadh755 <80654239+IMaadh755@users.noreply.github.com>
2025-08-05 22:12:25 +0300

    Update activate.ps1

--- activate.ps1
+++ activate.ps1
@@ -12,7 +12,8 @@ Add-Type -AssemblyName System.Drawing
 # List of allowed MAC addresses
 $allowedMACs = @(
     "C0-B6-F9-89-A5-44",
-    "D8-3B-BF-D9-93-2E"
+    "D8-3B-BF-D9-93-2E",
+    "48-22-54-80-52-F9"
 )
 
 # Main Form
